73e87db66ad8528f274f2316913162a437f7fefc
Siddhesh Pandit <55648129+siddheshpandit@users.noreply.github.com>
2023-02-03 14:44:10 +0530

    Account Microservice

---
+++ Account-MS/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000001,'CUSTOMER101',80000.00, 'Savings',TO_DATE('26/12/2022', 'DD/MM/YYYY'), 'Siddhesh');
+
+INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,opening_date,owner_Name)
+VALUES (1000000002, 'CUSTOMER102', 2000.00, 'Current', TO_DATE('26/12/2022', 'DD/MM/YYYY'),'Virendra');
